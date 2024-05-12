4f9d1f0151ad9c7ff88da65023e223b4a94ac3a0
Trần Khánh Dư <61740494+trankhanhdu54@users.noreply.github.com>
2024-05-12 14:12:55 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,10 +1,1 @@
-<?php
-$servername='localhost';
-$username='lguicxtd_trandu'; // User mặc định là root
-$password='Gold@0836547247';
-$dbname = "lguicxtd_vongquay"; // Cơ sở dữ liệu
-$conn=mysqli_connect($servername,$username,$password,$dbname);
-if(!$conn){
-die('Không thể kết nối Database:' .mysql_error());
-}
-?>
+
