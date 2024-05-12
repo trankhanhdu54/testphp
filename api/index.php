7d438e2e3fed86c0fa19428c49569784bff6edaa
Trần Khánh Dư <61740494+trankhanhdu54@users.noreply.github.com>
2024-05-12 15:19:08 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,1 +1,11 @@
+<?php
+$servername='sql11.freemysqlhosting.net:3306';
+$username='sql11705935'; // User mặc định là root
+$password='BVl9mLMdWI';
+$dbname = "lguicxtd_vongquay"; // Cơ sở dữ liệu
+$conn=mysqli_connect($servername,$username,$password,$dbname);
+if(!$conn){
+die('Không thể kết nối Database:' .mysql_error());
+}
 
+?>
